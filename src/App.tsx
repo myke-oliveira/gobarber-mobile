2d87611d6b887dd8de8404f131ef6d4b709b4e24
Myke Oliveira <myke.oliveira@monetizze.com>
2021-03-06 18:16:10 -0300

    🎉 INT COMMIT: start react-native

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {
+  View,
+  StatusBar,
+} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <View />
+  );
+};
+
+export default App;
